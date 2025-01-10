c7099595b5f6ef61db5882d04e07aef8623dcc60
AnithaS123 <hani10.zenith@gmail.com>
2025-01-10 22:51:47 +0400

    Initial commit

--- try.ps1
+++
@@ -1,3 +0,0 @@
-$password = "MyStr0ngP@$$wOrd2024"
-$password = ConvertTo-SecureString -String "****" -AsPlainText -Force
-New-AzADUser -DisplayName SampleName -Password $password -AccountEnabled $true -MailNickname SampleNickName -UserPrincipalName sample@hani2024zenithgmail.onmicrosoft.com
